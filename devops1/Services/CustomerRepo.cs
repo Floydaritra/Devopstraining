ba9af0bcea6f2795c19b6408e2090845dc5d2b5a
Floydaritra <Floydaritra@users.noreply.github.com>
2022-08-30 01:54:40 +0530

    First commit

---
+++ devops1/Services/CustomerRepo.cs
@@ -0,0 +1,23 @@
+﻿using devops1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace devops1.Services
+{
+    public class CustomerRepo : IRepo<int, Customer>
+    {
+        static List<Customer> _customer = new List<Customer>()
+        {
+            new Customer(){ Id = 1, Name = "Aritra", Age = 21 }
+        };
+        public async Task<Customer> Add(Customer item)
+        {
+            _customer.Add(item);
+            return item;
+        }
+        public async Task<Customer> GetAll(Customer item)
+        {
+            return _customers;
+        }
+    }
+}
---
+++ devops1/Services/IRepo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace devops1.Services
+{
+    public interface IRepo<K,T>
+    {
+        public Task<T> Add(T item);
+        public Task<T> Update(T item);
+        public Task<ICollection<T>> GetAll();
+    }
+}
